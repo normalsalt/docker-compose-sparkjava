30386ec3d63c98ac4c1a610b59fff17375453787
normalsalt <normalsalt@gmail.com>
2021-01-17 17:25:28 +0900

    add src/main/resources/public/index.html

--- src/main/java/sparkjava/Main.java
+++ src/main/java/sparkjava/Main.java
@@ -3,6 +3,15 @@ package sparkjava;
 import static spark.Spark.options;
 import static spark.Spark.before;
 
+import static spark.Spark.staticFiles;
+import static spark.Spark.post;
+import javax.servlet.MultipartConfigElement;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
 import static spark.Spark.get;
 import static spark.Spark.notFound;
 import static spark.Spark.internalServerError;
@@ -25,13 +34,26 @@ public class Main {
       response.header("Access-Control-Allow-Origin", origin);
       response.header("Access-Control-Request-Method", methods);
       response.header("Access-Control-Allow-Headers", headers);
-      // Note: this may or may not be necessary in your particular application
+      // this may or may not be necessary in your particular application
       response.type("application/json");
     });
   }
 
   public static void main(String[] args) {
-    get("/", (req, res) -> {
+    // root is 'src/main/resources', so put files in 'src/main/resources/public'
+    staticFiles.location("/public");
+    // http://sparkjava.com/documentation#examples-and-faq
+    post("/upload", (request, response) -> {
+      response.type("text/plain");
+      request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+      String text = "";
+      try(InputStream is = request.raw().getPart("uploaded_file").getInputStream()){
+        text = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+      }
+      return text;
+    });
+
+    get("/hello", (req, res) -> {
       res.type("application/json");
       return "{\"message\":\"Welcome to Spark Framework API!\"}";
     });
